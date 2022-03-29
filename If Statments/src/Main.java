c4df5354fe7e455f7907b7704bf11cdeb800f9bf
jsvillamayor <78101377+jsvillamayor@users.noreply.github.com>
2022-03-29 15:13:46 -0700

    Add files via upload

---
+++ If Statments/src/Main.java
@@ -0,0 +1,36 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int age = 14;
+		
+		if(age>=75) {
+			
+			System.out.println("Ok boomer.");
+			
+		}
+		
+		else if(age>=18) {
+			
+			System.out.println("You are an adult.");
+			
+		}
+		
+		else if (age >= 13) {
+			
+			System.out.println("You are a teenager.");
+			
+		}
+		
+		else {
+			
+			System.out.println("You are young.");
+			
+		}
+		
+		
+
+	}
+
+}
